d2b1777086f966d86e3a2f22d2be1f58b22800eb
Alexander Wilson <acwilson96@gmail.com>
2017-09-10 18:33:08 +0100

    Structure set up for server.

--- server.js
+++ server.js
@@ -1,13 +1,31 @@
-// Start express.
 var express = require('express');
-var app     = express();
+var http = require('http')
+var socketio = require('socket.io');
 
+var app = express();
+var server = http.Server(app);
+var websocket = socketio(server, { origins: '*:*'});
 
+server.listen(3000, () => console.log('listening on *:3000'));
 
-console.log('Server started.')
+/***************************************\
+            Server Web App
+\***************************************/
 
-app.get('/', function (req, res) {
-    res.send('Hello World')
+app.get('/', (req, res) => {
+    res.send('Hello World');
 });
 
-app.listen(3000);
+/***************************************\
+                  API
+\***************************************/
+
+// Connect
+websocket.on('connection', (socket) => {
+    socket.emit('notification', 'Hello world!');
+    console.log('A client just joined on', socket.id);
+});
+
+websocket.on('authenticate', (data) => {
+    console.log('auth')
+});
