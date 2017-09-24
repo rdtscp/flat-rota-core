765b795733acf9aa8b66c7953883b562087a55b5
Alex Wilson <acwilson96@protonmail.com>
2017-09-24 16:00:23 +0100

    Topup/Login/Register methods, render items on html page

--- code.js
+++ code.js
@@ -1,29 +1,79 @@
 function getResources() {
     axios.get('/resource/all')
     .then(res => {
-        items = res.data;
+        var items = res.data;
+        authToken = localStorage.getItem("authToken");
         items.forEach((element) => {
             console.log(element);
+            var ID = element._id;
+
             // Create element container.
             var cont = document.createElement("div");
             cont.classList.add("item-cont");
 
             // Create element info DIV.
             var node = document.createElement("div");
-            node.innerHTML = element.name + " <br> £" + element.price + " <br> Desc: " + element.description + " <br>Quantity: " + element.quantity + " <br> Rota: " + element.rota;
+            node.innerHTML = "Name: " + element.name + " <br> Price: £" + element.price + " <br> Desc: " + element.description + " <br>Quantity: " + element.quantity + " <br> Rota: " + element.rota;
             
-            // Create form.
-            var form = document.createElement("form");
+            // Create button.
             var topup = document.createElement("button");
-            form.appendChild(topup);
+            topup.setAttribute("onClick", "topup('" + ID + "', '" + authToken + "');");
+            topup.innerHTML = "Topup"
+
             
 
             cont.appendChild(node);
-            cont.appendChild(form);
+            cont.appendChild(topup);
+            // cont.appendChild(form);
             var element = document.getElementById("list");
             element.appendChild(cont);
         });
     });
 }
 
+function topup(id, authToken) {
+    alert('Topped Up!');
+    axios.post('/resource/topup', {
+        id: id,
+        authToken: authToken,
+        quantity: 1
+    })
+    .then(res => {
+        console.log(res.data)
+        if (res.data.msg) alert(res.data.msg)
+        window.location.reload();
+    })
+}
+
+function login() {
+    var uname = document.getElementById('log_uname').value;
+    var pword = document.getElementById('log_pword').value;
+    axios.post('/login', {
+        username: uname,
+        password: pword
+    }).then(res => {
+        if (res.data.authToken) {
+            localStorage.setItem("authToken", res.data.authToken);
+        }
+    })
+
+}
+
+function register() {
+    var uname = document.getElementById('reg_uname').value;
+    var pword = document.getElementById('reg_pword').value;
+    axios.post('/register', {
+        username: uname,
+        password: pword
+    }).then(res => {
+        console.log(res.data)
+        if (res.data.authToken) {
+            localStorage.setItem("authToken", res.data.authToken);
+        }
+    })
+}
+
 getResources();
+
+var authToken = localStorage.getItem("authToken");
+console.log(authToken)
