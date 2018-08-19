23415e525f340b2ad49fd7645952937ae9bdcdaa
Alexander Wilson <acwilson96@gmail.com>
2018-08-19 18:04:10 +0100

    Importance Tracking Attributes

--- api/models/Item.js
+++ api/models/Item.js
@@ -24,6 +24,14 @@ module.exports = {
 
     rota: {
       type:       'string'
+    },
+
+    notification: {
+      type:       'boolean'
+    },
+
+    lastBumped: {
+      type:       'number'
     }
 
   },
