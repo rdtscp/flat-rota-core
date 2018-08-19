f5975dc1df16f4d251ed3157dfa24d1f66601b37
Alexander Wilson <acwilson96@gmail.com>
2018-08-19 11:57:24 +0100

    Item model

---
+++ api/models/Item.js
@@ -0,0 +1,32 @@
+/**
+ * Item.js
+ *
+ * @description :: A model definition.  Represents a database table/collection/etc.
+ * @docs        :: https://sailsjs.com/docs/concepts/models-and-orm/models
+ */
+
+module.exports = {
+
+  attributes: {
+
+    flat: {
+      model: 'Flat',
+    },
+
+    name: {
+      type:       'string',
+      required:   true
+    },
+
+    description: {
+      type:       'string'
+    },
+
+    rota: {
+      type:       'string'
+    }
+
+  },
+
+};
+
